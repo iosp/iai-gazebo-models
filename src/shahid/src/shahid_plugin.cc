f33c222d4f98a1206173ce5691c634fd74736ac0
daniel <daniel.meltz@gmail.com>
2017-09-04 17:51:19 +0300

    improve in the shahid controller

--- src/shahid/src/shahid_plugin.cc
+++ src/shahid/src/shahid_plugin.cc
@@ -26,21 +26,31 @@ namespace gazebo
 
       this->Reset();
       
-      //Read in the animation factor (applied in the OnUpdate function).
+      //Read in the shahid_name from the sdf file
       if (_sdf->HasElement("shahid_name"))
-        this->actor_name = _sdf->Get<std::string>("shahid_name");
+        this->actor_Name = _sdf->Get<std::string>("shahid_name");
       else
-        this->actor_name = "jon";
+        this->actor_Name = "musa";
 
-      this->actor_sylinder = this->world->GetModel(this->actor_name + "_cylinder_model");
-        
+      //Read in the animationFrameRate from the sdf file
+      if (_sdf->HasElement("animationFrameRate"))
+        this->animationFrameRate = _sdf->Get<double>("animationFrameRate");
+      else
+        this->animationFrameRate = 30.0;
+
+
+      // conecting to the BoundingCylinder   
+      this->actor_BoundingCylinder_ModelName = this->actor_Name + "_boundingCylinder_model";
+      this->actor_BoundingCylinder_LinkName = this->actor_Name+"_boundingCylinder_link";
+      this->actor_BoundingCylinder = this->world->GetModel(this->actor_BoundingCylinder_ModelName);
+
  
 
       this->Ros_nh = new ros::NodeHandle("shahid_actor_node");
       
       // Subscribe to the topic, and register a callback
-      target_pose_sub = this->Ros_nh->subscribe("/"+actor_name+"/target_pose", 60, &shahidPlugin::Newtarget_pose, this);
-         
+      actorTargetPose_sub = this->Ros_nh->subscribe("/"+actor_Name+"/target_pose", 60, &shahidPlugin::newActorTarget, this);
+      
 
       // Listen to the update event. This event is broadcast every simulation iteration.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(
@@ -51,10 +61,10 @@ namespace gazebo
 /////////////////////////////////////////////////
     void Reset()
     {
-      this->velocity = 1.0;
-      this->lastUpdate = 0;
+      this->actor_TargetVel = 1.0;
+      this->lastUpdateAnimation = 0;
      
-      this->target_pose = ignition::math::Vector3d(this->actor->WorldPose().Pos());
+      this->actor_TargetPose = ignition::math::Vector3d(this->actor->WorldPose().Pos());
       
 
       ignition::math::Pose3d actorPose = this->actor->WorldPose();
@@ -80,11 +90,11 @@ namespace gazebo
     }
 
 /////////////////////////////////////////////////
-    void Newtarget_pose(const geometry_msgs::PointPtr &msg)
+    void newActorTarget(const geometry_msgs::PointPtr &msg)
     {
-      this->target_pose.X(msg->x);
-      this->target_pose.Y(msg->y);
-      this->velocity = msg->z;
+      this->actor_TargetPose.X(msg->x);
+      this->actor_TargetPose.Y(msg->y);
+      this->actor_TargetVel = msg->z;     
     }
 
 
@@ -92,81 +102,81 @@ namespace gazebo
     public: void OnUpdate(const common::UpdateInfo & _info)
     {
       // Time delta
-      double dt = (_info.simTime - this->lastUpdate).Double();
-      this->lastUpdate = _info.simTime;
-      
-      ignition::math::Pose3d actorPose = this->actor->WorldPose();
-      //std::cout << "actor  pose :   x = " << actorPose.Pos().X() << " y = " << actorPose.Pos().Y() << std::endl;
-      //std::cout << "target pose :   x = " << this->target_pose.X() << " y = " << this->target_pose.Y() << std::endl;
-      
+      double dtAnimation = (_info.simTime - this->lastUpdateAnimation).Double();
+      if (dtAnimation >= 1/animationFrameRate )
+       {
+        
+          this->lastUpdateAnimation = _info.simTime;
 
-   
+          ignition::math::Pose3d actorPose = this->actor->WorldPose();
 
+          ignition::math::Vector3d actorRPY = actorPose.Rot().Euler();
+          ignition::math::Vector3d targetDiss = this->actor_TargetPose - actorPose.Pos();
 
-      ignition::math::Vector3d actorRPY = actorPose.Rot().Euler();
-      ignition::math::Vector3d targetDiss = this->target_pose - actorPose.Pos();
-
-      if (targetDiss.Length() < 0.5)
-      {   
-        return;
-      }
-
-      ignition::math::Vector3d targetDirection = targetDiss.Normalize(); 
-      ignition::math::Angle yaw = atan2(targetDirection.Y(), targetDirection.X()) + 1.5707 - actorRPY.Z();
-      yaw.Normalize();
-
-      ignition::math::Pose3d actorNextPose = actorPose;
-      actorNextPose.Rot() = ignition::math::Quaterniond(1.5707, 0, actorRPY.Z() + yaw.Radian());
-      double disTraveled = 0;
-
-
-      // Rotate in place, instead of jumping.
-      if (std::abs(yaw.Radian()) > IGN_DTOR(10))
-          {
-            actorNextPose.Rot() = ignition::math::Quaterniond(1.5707, 0, actorRPY.Z() + yaw.Radian()*0.01);
-          }
-      else
-          {
-            actorNextPose.Pos() = actorPose.Pos() + targetDirection * this->velocity * dt;
-            disTraveled = (this->velocity * dt).Length();
-          }
-
-      //std::cout << "next pose :   x = " << actorNextPose.Pos().X() << " y = " << actorNextPose.Pos().Y() << std::endl;
           
+          if (targetDiss.Length() < 0.5)
+          {   
+            return;
+          }
 
-      // Distance traveled is used to coordinate motion with the walking animation
-      this->actor->SetWorldPose(actorNextPose, false, false);
-      this->actor->SetScriptTime(this->actor->ScriptTime() + (disTraveled * this->animationFactor));
-      
-      
-     ignition::math::Pose3d cylinder_NextPose = actorNextPose;
-     cylinder_NextPose.Rot() = ignition::math::Quaterniond( 0 , 0, 0);
-     this->actor_sylinder->SetLinkWorldPose(cylinder_NextPose, this->actor_name+"_cylinder_link");
+          ignition::math::Vector3d targetDirection = targetDiss.Normalize(); 
+          ignition::math::Angle yaw = atan2(targetDirection.Y(), targetDirection.X()) + 1.5707 - actorRPY.Z();
+          yaw.Normalize();
+
+          ignition::math::Pose3d actorNextPose = actorPose;
+          actorNextPose.Rot() = ignition::math::Quaterniond(1.5707, 0, actorRPY.Z() + yaw.Radian());
+          double disTraveled = 0;
+
+          
+          // Rotate in place, instead of jumping.
+          if (std::abs(yaw.Radian()) > IGN_DTOR(10))
+              {
+                actorNextPose.Rot() = ignition::math::Quaterniond(1.5707, 0, actorRPY.Z() + yaw.Radian()*dtAnimation*this->actor_TargetVel.Length());
+              }
+          else
+              {
+                actorNextPose.Pos() = actorPose.Pos() + targetDirection * this->actor_TargetVel * dtAnimation;
+                disTraveled = (this->actor_TargetVel * dtAnimation).Length();
+              }
+              
+              
+          // Distance traveled is used to coordinate motion with the walking animation
+          this->actor->SetWorldPose(actorNextPose, false, false);
+          this->actor->SetScriptTime(this->actor->ScriptTime() + (disTraveled * this->animationFactor));
+                
+
+          // moving the bounding Cylinder 
+          ignition::math::Pose3d boundingCylinder_NextPose = actorNextPose;
+          boundingCylinder_NextPose.Rot() = ignition::math::Quaterniond( 0 , 0, 0);
+          this->actor_BoundingCylinder->SetLinkWorldPose(boundingCylinder_NextPose, this->actor_BoundingCylinder_LinkName);
+                    
+        }
     }
 
 
       
     private: ros::NodeHandle *Ros_nh;  // Defining private Ros Node Handle
-    private: ros::Subscriber target_pose_sub; // Defining private Ros Subscribers
+    private: ros::Subscriber actorTargetPose_sub; // Defining private Ros Subscribers
     
-    private: std::string actor_name;   
+    private: std::string actor_Name;   
 
-
-    private: physics::ModelPtr actor_sylinder;
-
+    private: physics::ModelPtr actor_BoundingCylinder;
+    private: std::string actor_BoundingCylinder_ModelName;
+    private: std::string actor_BoundingCylinder_LinkName;
 
     private: event::ConnectionPtr updateConnection;    // Pointer to the update event connection
 
     private: physics::ActorPtr actor;
     private: physics::WorldPtr world;
     private: sdf::ElementPtr sdf;
-    private: ignition::math::Vector3d target_pose;
 
-    private: common::Time lastUpdate;
-    private: ignition::math::Vector3d velocity; // Velocity of the actor
+    private: ignition::math::Vector3d actor_TargetPose;
+    private: ignition::math::Vector3d actor_TargetVel; // actor_TargetVel of the actor
 
+    private: common::Time lastUpdateAnimation;
+    private: double animationFrameRate;
+  
     private: physics::TrajectoryInfoPtr trajectoryInfo; //Custom trajectory info.
-    
     private: double animationFactor = 4.5; // Time scaling factor. Used to coordinate translational motion with the actor's walking animation.
   };
 
